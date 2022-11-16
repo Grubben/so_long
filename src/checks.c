cbd10c005a67183ac4da55931277b670cb7edfee
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r6s1.42lisboa.com>
2022-11-16 15:38:39 +0000

    checks: norminetted

--- src/checks.c
+++ src/checks.c
@@ -6,19 +6,18 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:35:23 by endarc            #+#    #+#             */
-/*   Updated: 2022/11/15 18:26:37 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:38:30 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 // Checks wether map is rectangular and closed by walls
-int mtrx_checkwallsp(char **mtrxmap)
+int	mtrx_checkwallsp(char **mtrxmap)
 {
 	size_t	lenx;
-	size_t  j;
-	
-	// Not necessary. Already checked in main:100
+	size_t	j;
+
 	if (!mtrxmap[0])
 		return (0);
 	lenx = ft_strlen(mtrxmap[0]);
@@ -27,27 +26,22 @@ int mtrx_checkwallsp(char **mtrxmap)
 	j = 1;
 	while (mtrxmap[j] != NULL)
 	{
-		// Checks if rectangular
 		if (ft_strlen(mtrxmap[j]) != lenx)
 			return (0);
-		// Checks if sided by walls
-		if (mtrxmap[j][0] != WALL || mtrxmap[j][lenx-1] != WALL)
+		if (mtrxmap[j][0] != WALL || mtrxmap[j][lenx -1] != WALL)
 			return (0);
 		j++;
 	}
-	// Checks if first and last row is all WALL
-	if (!ft_str_isallp(mtrxmap[0], WALL) || !ft_str_isallp(mtrxmap[j-1], WALL))
+	if (!ft_str_isallp(mtrxmap[0], WALL) || !ft_str_isallp(mtrxmap[j -1], WALL))
 		return (0);
 	return (1);
 }
-
 
 int	checkconsts(char c, void *hiddendata, size_t i, size_t j)
 {
 	int	**data;
 
 	data = hiddendata;
-	
 	(void)i;
 	(void)j;
 	if (c == EXIT)
@@ -64,7 +58,6 @@ int	checkstartpos(char c, void *hiddendata, size_t i, size_t j)
 	t_info	*data;
 
 	data = hiddendata;
-	
 	if (c == COLLECT)
 	{
 		data->n_collectibles++;
@@ -77,7 +70,6 @@ int	checkstartpos(char c, void *hiddendata, size_t i, size_t j)
 		data->matrixmap[j][i] = EMPTY;
 		return (1);
 	}
-	// if c is not one of the consts map is broken
 	else if (c == EMPTY || c == WALL || c == EXIT)
 		return (1);
 	return (0);
@@ -87,24 +79,19 @@ int	mtrx_checkmp(t_info *worldata)
 {
 	int	pn;
 	int	en;
-	int *parr[2];
+	int	*parr[2];
 
 	pn = 0;
 	en = 0;
-
 	parr[0] = &pn;
 	parr[1] = &en;
-
 	if (!func_charmtrxdo(worldata->matrixmap, &parr, checkconsts))
 		return (0);
-	// if (!mtrx_checktiles(worldata, &pn, &en))
-	if (!func_charmtrxdo(worldata->matrixmap, worldata, checkstartpos))	
+	if (!func_charmtrxdo(worldata->matrixmap, worldata, checkstartpos))
 		return (0);
-
 	if (!worldata->n_collectibles)
 		return (0);
 	if (en != 1 || pn != 1)
 		return (0);
 	return (1);
-
 }
